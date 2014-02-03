b21f2c2d22997489a2021d3667b029d9b8125b65
Alumno <alumno@localhost.localdomain>
2014-02-03 08:41:54 -0600

    Primer Comit

---
+++ src/Prueba.java
@@ -0,0 +1,15 @@
+/*import java.util.Scanner;
+
+public class Prueba {
+	public static void main (String ags[])
+	{
+		System.out.print ("Ingrese un numero:");
+
+		Scanner miprueba =new Scanner(System.in);
+
+		int num= miprueba.nextInt();;
+		System.out.print ("Ingresaste el numero:"+num);
+	}
+}
+*/
+
---
+++ src/Variables.java
@@ -0,0 +1,18 @@
+
+
+public class Variables {
+	
+public static void main (String args[])
+{
+
+	
+	int nu1=10;
+	int nu2=20;
+	
+	System.out.print(nu1+nu2);
+	
+}
+}
+
+	
+
